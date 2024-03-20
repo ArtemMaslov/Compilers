792c6cd232ce7ddd9493d3237a6b9bf0f6a57b3e
Konstantin Vikhlyantsev <vihlancevk@gmail.com>
2024-03-20 23:26:46 +0300

    Add new test for df

--- tests/dominance_frontier/test.py
+++ tests/dominance_frontier/test.py
@@ -9,22 +9,25 @@ codeDirPath = os.path.abspath(thisDirPath + "/../..")
 sys.path.append(codeDirPath)
 
 
-from typing import List
+from typing import List, Set, Dict
 import ui
 
 from dominance_frontier import DominanceFrontier
 
 
-file_names: List[str] = ['dominance_frontier_1.json']
+tests: Dict[str, List[Set[int]]] = {
+	'dominance_frontier_1.json': [set(), {1}, {7}, {7}, {6}, {6}, {7}, {1}],
+	'dominance_frontier_2.json': [set(), set(), {2}, set(), {2}, {2}, set()]
+}
 
 
-def run_test(file_name: str) -> None:
+def run_test(file_name: str, expected_value: List[Set[int]]) -> None:
 	ui.ColoredPrint(f'#c********************   #y{file_name}#c   ********************#rs')
 	
 	file_path: str = os.path.join(thisDirPath, file_name)
 	df, info = DominanceFrontier.value_of(file_path).calculate()
 
-	is_passed: bool = df == [set(), {1}, {7}, {7}, {6}, {6}, {7}, {1}]
+	is_passed: bool = df == expected_value
 
 	if is_passed:
 		ui.ColoredPrint("#gTest passed!#rs")
@@ -36,5 +39,5 @@ def run_test(file_name: str) -> None:
 	
 
 if __name__ == '__main__':
-	for file_name in file_names:
-		run_test(file_name)
+	for file_name, expected_value in tests.items():
+		run_test(file_name, expected_value)
